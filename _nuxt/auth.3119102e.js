0b7243d6ab96c59d6b71ae9f29c6911552df18a6
saikksub <saikksub@gmail.com>
2023-06-07 08:19:21 +0530

    init with build

---
+++ _nuxt/auth.3119102e.js
@@ -0,0 +1,1 @@
+import{K as e,b as s,r as l,o as r}from"./entry.76a0a7fb.js";const a={class:"flex flex-col justify-center min-h-full py-12"},u={__name:"auth",setup(o){return e({title:"Ghost Asset Browser",htmlAttrs:{},bodyAttrs:{class:"w-full h-full text-neutral-300 bg-gray-800"}}),(t,c)=>(r(),s("div",a,[l(t.$slots,"default")]))}};export{u as default};
